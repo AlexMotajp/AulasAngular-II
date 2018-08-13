8ea3235530e958757d4623761096090dd6df4551
AlexMotajp <alexandre.nike10@hotmail.com>
2018-08-13 13:01:35 -0300

    Aulas 02 Projeto segunda parte

---
+++ Exercicios/TypeScript01/MainTest.ts
@@ -0,0 +1,32 @@
+
+// javascript 
+var minhaVar = 'Hello World';
+
+function minhaFuncaoInicial(x, y) {
+    return x + y;
+}
+
+// Ecmascript
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3]
+numeros.map(function(valor) {
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2); // ES 2015
+
+class Matematica {
+    soma(x, y){
+        return x + y;
+    }
+}
+
+function funcaoBasic(x, y){
+    var z = 2;
+    
+    return 1;
+}
+
---
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -0,0 +1,20 @@
+import { CursosService } from './cursos.service';
+import { Component, OnInit } from '@angular/core';
+ 
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.css']
+})
+export class CursosComponent {
+
+  nomePortal: String;
+
+  cursos : String[];
+
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = 'http://loiane.training';   
+
+    this.cursos = this.cursosService.getCursos();
+  }
+}
