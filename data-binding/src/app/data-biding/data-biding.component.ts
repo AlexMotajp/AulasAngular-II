76803a308dc0a0c1103503894eb336e14ea20299
AlexMotajp <alexandre.nike10@hotmail.com>
2018-11-30 11:36:20 -0200

    Data Binding e Property

--- data-binding/src/app/data-biding/data-biding.component.ts
+++ data-binding/src/app/data-biding/data-biding.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBidingComponent implements OnInit {
 
+  url: String = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem = 'https://picsum.photos/400/200/?random';
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
   constructor() { }
 
   ngOnInit() {
