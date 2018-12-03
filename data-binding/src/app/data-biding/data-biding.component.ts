7b29c0a92a89c70b7679a6bf0f98510c72c976f8
AlexMotajp <alexandre.nike10@hotmail.com>
2018-12-03 18:57:15 -0200

    Aula não finalizada... Event Binding

--- data-binding/src/app/data-biding/data-biding.component.ts
+++ data-binding/src/app/data-biding/data-biding.component.ts
@@ -10,12 +10,22 @@ export class DataBidingComponent implements OnInit {
   url: String = 'http://loiane.com';
   cursoAngular: Boolean = true;
   urlImagem = 'https://picsum.photos/400/200/?random';
+
+  valorAtual: String = '';
   getValor() {
     return 1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('botaoClicado');
+  }
+
+  onKeyUp(evento) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
   }
   constructor() { }
 
